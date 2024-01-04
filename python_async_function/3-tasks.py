7081d3003dd9eb3bd8e13bc280f996a3c5691243
seeyou7 <>
2024-01-04 06:13:51 -0800

    function that creates an asyncio task

---
+++ python_async_function/3-tasks.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""Define a function that creates an asyncio task"""
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """Return an asyncio task"""
+    task = asyncio.create_task(wait_random(max_delay))
+    return task
