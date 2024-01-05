82245a9d3d20e64cf71aa2bf94fbdb96ba443705
seeyou7 <>
2024-01-05 08:23:16 -0800

    fix pycode errors

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-""" list of float & integer """
+"""Complex types - mixed list"""
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
-    """ Summation function for a list of floats and/or integers. """
-    result: float = 0
-    for number in mxd_lst:
-        result += number
-    return result
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """ define type-annotated function sum_mixed_list"""
+    sum: float = 0
+    for i in mxd_lst:
+        sum += i
+    return sum
