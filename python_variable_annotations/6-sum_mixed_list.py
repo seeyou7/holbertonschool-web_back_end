57659b3e47e9b5feea1cd5c3bf1531d87656bc5f
seeyou7 <>
2024-01-05 08:22:06 -0800

    fix pycode errors

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -4,8 +4,8 @@ from typing import List, Union
 
 
 def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
-        """ Summation function for a list of floats and/or integers. """
-        result: float = 0
-        for number in mxd_lst:
-            result+= number
-        return result
+    """ Summation function for a list of floats and/or integers. """
+    result: float = 0
+    for number in mxd_lst:
+        result += number
+    return result
