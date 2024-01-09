fdd44597e4f083248d82d28547e7e0c978ad5bc1
seeyou7 <>
2024-01-09 01:15:46 -0800

    add extra empty line

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
-""" fuc to list all doc in a collection"""
+"""list all documents in a collection"""
 
 from pymongo import MongoClient
 
 
 def list_all(mongo_collection):
-    """ return list of doc or emty list if no doc"""
+    """Return a list of docs if not empty"""
     result = mongo_collection.school.find()
     if result:
         return result
     return []
-
