c29116ee86abf491fa151a327f297f19502048f5
seeyou7 <5764@holbertonstudents.com>
2024-01-16 14:02:49 +0100

    fix eslint errors

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,11 +1,12 @@
 export default function taskBlock(trueOrFalse) {
-    var task = false;
-    var task2 = true;
-  
-    if (trueOrFalse) {
-      let task = true;
-      let task2 = false;
-    }
-  
-    return [task, task2];
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    /* eslint-disable no-unused-vars */
+    const task = true;
+    const task2 = false;
+    /* eslint-enable no-unused-vars */
   }
+  return [task, task2];
+}
