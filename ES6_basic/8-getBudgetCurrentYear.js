32fdaf7ca009d3ddbeebc2b92e8e84a9d2d4715b
seeyou7 <5764@holbertonstudents.com>
2024-01-16 17:10:24 +0100

    change the code to define the property names dynamically in the return

--- ES6_basic/8-getBudgetCurrentYear.js
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -1,13 +1,12 @@
-function getCurrentYear(date = new Date()) {
+function getCurrentYear() {
+  const date = new Date();
   return date.getFullYear();
 }
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
-  const budget = {};
-
-  budget[`income-${getCurrentYear()}`] = income;
-  budget[`gdp-${getCurrentYear()}`] = gdp;
-  budget[`capita-${getCurrentYear()}`] = capita;
-
-  return budget;
+  return {
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
+  };
 }
