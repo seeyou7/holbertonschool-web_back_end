6e73d2ffdee6508d0f70607cfae03195d27dfd60
seeyou7 <5764@holbertonstudents.com>
2024-01-26 18:14:14 +0100

    Handle multiple promises from other files

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,9 +1,10 @@
-// Handle multiple promises
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
-  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)]);
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => (results.map((result) => ({
+      status: result.status,
+      value: result.value ? result.value : String(result.reason),
+    }))));
 }
-
-export default handleProfileSignup;
