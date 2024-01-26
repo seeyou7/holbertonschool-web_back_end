09515096e19758e78a5e1a49e9c92b3d56509fa4
seeyou7 <5764@holbertonstudents.com>
2024-01-26 17:57:15 +0100

    change the code to return the promise

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,10 +1,8 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default async function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then((results) => {
-      console.log(results[0].body, results[1].firstName, results[1].lastName);
-    })
-
+export default function handleProfileSignup() {
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((value) => console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`))
     .catch(() => console.log('Signup system offline'));
 }
