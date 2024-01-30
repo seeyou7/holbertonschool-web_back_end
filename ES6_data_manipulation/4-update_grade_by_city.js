db1272e1133b82a7492f87d03c32cb65c4d83fe5
seeyou7 <5764@holbertonstudents.com>
2024-01-30 16:24:53 +0100

    combine filter and map

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,15 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+    return students
+      .filter((student) => student.location === city)
+      .map((student) => {
+        const matchingGrade = newGrades.find((grade) => grade.studentId === student.id);
+  
+        return {
+          id: student.id,
+          firstName: student.firstName,
+          location: student.location,
+          grade: matchingGrade ? matchingGrade.grade : 'N/A'
+        };
+      });
+  }
+  
