6d340dcbe8997ba2ec860933c878aaf5c036164c
seeyou7 <5764@holbertonstudents.com>
2024-01-30 16:28:08 +0100

    fix syntax style

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,15 +1,14 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-    return students
-      .filter((student) => student.location === city)
-      .map((student) => {
-        const matchingGrade = newGrades.find((grade) => grade.studentId === student.id);
-  
-        return {
-          id: student.id,
-          firstName: student.firstName,
-          location: student.location,
-          grade: matchingGrade ? matchingGrade.grade : 'N/A'
-        };
-      });
-  }
-  
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const matchingGrade = newGrades.find((grade) => grade.studentId === student.id);
+
+      return {
+        id: student.id,
+        firstName: student.firstName,
+        location: student.location,
+        grade: matchingGrade ? matchingGrade.grade : 'N/A',
+      };
+    });
+}
