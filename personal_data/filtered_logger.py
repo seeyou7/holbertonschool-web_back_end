01b8f3b029bcf321bed287130f5c576a389d8dad
seeyou7 <5764@holbertonstudents.com>
2024-10-02 14:41:23 +0200

    fix pycidestyle

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,6 +4,5 @@ import re
 
 def filter_datum(fields, redaction, meassage, separator):
     """Returns the obfuscated log message."""
-
     pattern = r'(' + '|'.join(fields) + r')=([^' + separator + r']*)'
     return re.sub(pattern, r'\1=' + redaction, meassage)
