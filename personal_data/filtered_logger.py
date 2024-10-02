7e8a5fdee4e63a4a4ba0b4a422aab380d906c0e4
seeyou7 <5764@holbertonstudents.com>
2024-10-02 14:34:04 +0200

    regex pattern

---
+++ personal_data/filtered_logger.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import re
+
+
+def filter_datum(fields, redaction, meassage, separator):
+    """Returns the obfuscated log message."""
+
+    pattern = r'(' + '|'.join(fields) + r')=([^' + separator + r']*)'
+    return re.sub(pattern, r'\1=' + redaction, meassage)
