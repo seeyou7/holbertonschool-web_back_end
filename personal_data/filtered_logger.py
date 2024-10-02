47ae18992264544424c81533a810b70e3e2fc565
seeyou7 <5764@holbertonstudents.com>
2024-10-02 15:56:57 +0200

    logging example with sensitive fields Log formatter

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,10 +1,35 @@
 #!/usr/bin/env python3
 import re
-from typing import List
+import logging
 
 
-def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str) -> str:
-    """Returns the obfuscated log message."""
-    pattern = f'({"|".join(fields)})=([^${separator}]+)'
-    return re.sub(pattern, rf'\1={redaction}', message)
+def filter_datum(fields, redaction, message, separator):
+    """
+     Replaces the values of specified fields in a
+     log message with a redaction string.
+    """
+    for field in fields:
+        message = re.sub(
+            f'{field}=[^ {separator}]+', f'{field}={redaction}', message
+        )
+    return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: list):
+        """Constructor that accepts fields to be redacted."""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Filter sensitive information from log records."""
+        original_message = super().format(record)
+        # Get the original log message
+        return filter_datum(self.fields, self.REDACTION,
+                            original_message, self.SEPARATOR)
