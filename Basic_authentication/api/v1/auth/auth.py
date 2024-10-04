1b44086bebdc209950fa8b0da25f463b16993707
seeyou7 <5764@holbertonstudents.com>
2024-10-04 13:37:02 +0200

    pycodestyle fixed

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -2,23 +2,24 @@
 
 from typing import List, TypeVar
 
+
 class Auth:
     """ Authentication class """
-    
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
         This method will later be used to determine if a request should be
         authenticated based on the path. For now, it just returns False.
         """
         return False
-    
+
     def authorization_header(self, request=None) -> str:
         """
         This method will return None for now. In the future, it will extract
         the authorization header from the request.
         """
         return None
-    
+
     def current_user(self, request=None) -> TypeVar('User'):
         """
         This method will return None for now. Later, it will return the current
