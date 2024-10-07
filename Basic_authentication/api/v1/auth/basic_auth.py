bb601c1c37568cfb43723487066368438130e927
seeyou7 <5764@holbertonstudents.com>
2024-10-07 10:28:00 +0200

    Inheritance and Polymorphism:

---
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+# api/v1/auth/basic_auth.py
+#!/usr/bin/env python3
+"""
+Basic Authentication module
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """ Basic authentication class that inherits from Auth.
+    """
+    pass
