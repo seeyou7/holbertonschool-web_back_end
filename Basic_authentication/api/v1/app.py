71f183baa54bcfa9e3a3ef6bcf3dd2f6e767528f
seeyou7 <5764@holbertonstudents.com>
2024-10-07 10:12:07 +0200

    Request validation!

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -8,7 +8,7 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
 # Import the Auth class
-from api.v1.auth.auth import Auth  
+from api.v1.auth.auth import Auth
 
 
 app = Flask(__name__)
