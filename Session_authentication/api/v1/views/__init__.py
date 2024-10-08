9d31b88d0c87da7198f65d8f3567f6942d24f521
seeyou7 <5764@holbertonstudents.com>
2024-10-08 13:16:26 +0200

    update files to avoid the circular import

--- Session_authentication/api/v1/views/__init__.py
+++ Session_authentication/api/v1/views/__init__.py
@@ -2,9 +2,12 @@
 """ Module for setting up Blueprint and views import. """
 
 from flask import Blueprint
-from api.v1.views.index import *
-from api.v1.views.users import *
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
-User.load_from_file()  # Assuming this is necessary to initialize users
+# Import views after app_views is defined to avoid circular import issues
+from api.v1.views.index import *
+from api.v1.views.users import *
+
+# Call any required initialization after all imports are complete
+User.load_from_file()  
