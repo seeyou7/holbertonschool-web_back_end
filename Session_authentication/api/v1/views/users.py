ccefd61d7536d04d9e180bd0dfae0338ccbd4bbd
seeyou7 <5764@holbertonstudents.com>
2024-10-08 12:44:07 +0200

    fixe code style & update user end point

--- Session_authentication/api/v1/views/users.py
+++ Session_authentication/api/v1/views/users.py
@@ -25,21 +25,10 @@ def view_one_user(user_id: str = None) -> str:
       - User object JSON represented
       - 404 if the User ID doesn't exist
     """
-
-    if user_id is None:
-        abort(404)
-
-    if user_id == "me" and request.current_user is None:
-        abort(404)
-
-    if user_id == "me" and request.current_user is not None:
+    if user_id == "me":
+        if request.current_user is None:
+            abort(404)
         return jsonify(request.current_user.to_json())
-
-    user = User.get(user_id)
-    if user is None:
-        abort(404)
-
-    return jsonify(user.to_json())
 
 
 @app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
@@ -129,3 +118,14 @@ def update_user(user_id: str = None) -> str:
         user.last_name = rj.get('last_name')
     user.save()
     return jsonify(user.to_json()), 200
+
+
+@app_views.route('/users/me', methods=['GET'], strict_slashes=False)
+def get_current_user():
+    """ GET /api/v1/users/me
+    Return:
+      - Authenticated User object JSON represented
+    """
+    if request.current_user is None:
+        abort(404)
+    return jsonify(request.current_user.to_json())
