fe71c8b68fbea70f432d1e3ee9abb9f777e8e587
seeyou7 <5764@holbertonstudents.com>
2024-10-16 18:19:42 +0200

    methods get user by sess ID

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -78,3 +78,14 @@ class Auth:
 
         self._db.update_user(user.id, session_id=session_id)
         return session_id
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """Retrieves a User object based on a session ID.
+        """
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
