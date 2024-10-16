6bbffa1e349aac3db2a9ca58bfff8a6e66079a3f
seeyou7 <5764@holbertonstudents.com>
2024-10-16 11:04:06 +0200

    user registration if already exist raise error

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -19,3 +19,31 @@ def _hash_password(password: str) -> bytes:
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
 
     return hashed_password
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ Registers a user with a given email and password.
+
+            Args:
+                email (str): The email address of the new user.
+                password (str): The plaintext password of the new user.
+
+            Returns:
+                User: The instance of the newly created User object.
+        """
+        try:
+            existing_user = self._db.find_user_by(email=email)
+            if existing_user:
+                raise ValueError("User {} already exists".format(email))
+
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            new_user = self._db.add_user(email, hashed_password)
+            return new_user
