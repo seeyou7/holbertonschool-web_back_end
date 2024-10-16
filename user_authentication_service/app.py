a87810433a9b7f2348f3ce4de53b2949aff04025
seeyou7 <5764@holbertonstudents.com>
2024-10-16 12:20:08 +0200

    basic Flask app

---
+++ user_authentication_service/app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""App module
+"""
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/', methods=["GET"])
+def welcome():
+    """GET route that returns a welcome 
+        message in JSON format.
+    """
+    return jsonify({"message": "Bienvenue"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
