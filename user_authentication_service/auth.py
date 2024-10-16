1aedeb6eafaa384ad1f7ff60cdc3e171e6742b3c
seeyou7 <5764@holbertonstudents.com>
2024-10-16 15:28:00 +0200

    generate a str representaation of UUID universal unique identifier

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -21,17 +21,18 @@ def _hash_password(password: str) -> bytes:
     return hashed_password
 
 
+def _generate_uuid() -> str:
+    """ Generates a new UUID and returns its string representation.
+    """
+    return str(uuid.uuid4())
+
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
 
     def __init__(self):
         self._db = DB()
-
-    def _generate_uuid() -> str:
-        """ Generates a new UUID and returns its string representation.
-        """
-        return str(uuid.uuid4())
 
     def register_user(self, email: str, password: str) -> User:
         """ Registers a user with a given email and password.
