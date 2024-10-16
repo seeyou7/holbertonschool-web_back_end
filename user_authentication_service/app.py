95a71f3797280deff840290c7d2a46ac359d23a9
seeyou7 <5764@holbertonstudents.com>
2024-10-16 12:21:40 +0200

    fix pep8 style

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -6,12 +6,14 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=["GET"])
 def welcome():
-    """GET route that returns a welcome 
+    """GET route that returns a welcome
         message in JSON format.
     """
     return jsonify({"message": "Bienvenue"})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
