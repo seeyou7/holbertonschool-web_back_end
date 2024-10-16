42f2bb4cb85bf0be6d3ca6a76ca374477f4bbeca
seeyou7 <5764@holbertonstudents.com>
2024-10-16 10:45:54 +0200

    hashing pswd using salt + bcrypt

---
+++ user_authentication_service/auth.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+    the auth module
+"""
+import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+import uuid
+
+
+
+def _hash_password(password: str) -> bytes:
+    """
+    Hashes a password with a randomly-generated salt
+    and returns the salted, hashed password.
+    """
+    salt = bcrypt.gensalt()
+
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+
+    return hashed_password
