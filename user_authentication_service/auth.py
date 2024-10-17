165f848d8c3e7094b9477bfdcd622d9246dcc30d
seeyou7 <5764@holbertonstudents.com>
2024-10-17 15:15:59 +0200

    update VAR name in auth & abort(403) to abort(400)

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -116,8 +116,8 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(reset_token=reset_token)
-            _hash_password = _hash_password(password)
-            self._db.update_user(user.id, _hashed_password=_hash_password,
+            hash_password = _hash_password(password)
+            self._db.update_user(user.id, hashed_password=hash_password,
                                  reset_token=None)
 
         except NoResultFound:
