717b560d9d218a1ffad66c62a83c24571cd744b2
seeyou7 <5764@holbertonstudents.com>
2024-10-17 10:57:05 +0200

    adding logout route app

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -53,5 +53,21 @@ def login():
         abort(401)
 
 
+@app.route('/sessions', methods=["DELETE"], strict_slashes=False)
+def logout():
+    """
+        Ennpoint for logout the user
+    """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        Auth.destroy_session(user.id)
+        response = make_response(redirect('/'))
+        response.set_cookies('session_id', '', expire=0)
+        return response
+    else:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
