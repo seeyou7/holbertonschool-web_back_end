98bebc926c09d4d62b41ec9fa79a720d060778b3
seeyou7 <5764@holbertonstudents.com>
2024-11-08 17:43:03 +0100

    aprocedure addbonus

---
+++ MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,15 @@
+-- script that creates a stored procedure AddBonus that adds a new correction for a student.
+
+DELIMITER //
+CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
+BEGIN
+    DECLARE project_id INT;
+    SET project_id = (SELECT id FROM projects WHERE name = project_name LIMIT 1);
+    IF project_id IS NULL THEN
+        INSERT INTO projects (name) VALUES (project_name);
+        SET project_id = LAST_INSERT_ID();
+    END IF;
+    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
+END;
+//
+DELIMITER ;
