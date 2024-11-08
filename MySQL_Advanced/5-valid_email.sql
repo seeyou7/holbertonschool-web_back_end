a4bde2f3d56868afa01aad230ca62e64618d1e90
seeyou7 <5764@holbertonstudents.com>
2024-11-08 17:33:09 +0100

    create trigger to check valid email with o or 1

---
+++ MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,15 @@
+-- Script that creates a trigger that resets the attribute valid_email only when the email has been changed.
+-- Task: 5. Who am I? - displays the current MySQL user
+-- Script can be executed on any MySQL server
+
+DELIMITER //
+CREATE TRIGGER reset_valid_email BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email != OLD.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END;
+//
+DELIMITER ;
+
