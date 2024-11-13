0e42b699231af617986abe079a3a2f920ee2e336
seeyou7 <5764@holbertonstudents.com>
2024-11-13 11:24:39 +0100

    Combining description using types & Describe

---
+++ unittests_in_js/1-calcul.test.js
@@ -0,0 +1,24 @@
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumber', function() {
+
+    describe('Test the calculateNumber sum, subtract and divide', function() {
+
+        it('should returnthe SUM OF ROUNDED NUMBER', function() {
+            assert.equal(calculateNumber('sum', 1.4, 4.5), 6);
+        });
+    
+        it('should return the Difference between a & b', function() {
+            assert.equal(calculateNumber('sub', 1.4, 4.5), -4);
+        });
+    
+        it('should return the Division of a & b', function() {
+            assert.equal(calculateNumber('division', 1.4, 4.5), 0.2);
+        });
+    
+        it('should return the Division by 0 shld throw error', function() {
+            assert.equal(calculateNumber('division', 1.2, 0), 'Error');
+        });
+});
+});
