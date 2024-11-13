64508884cc4f6326b3f4fee3e4281ff90806a687
seeyou7 <5764@holbertonstudents.com>
2024-11-13 11:54:33 +0100

    add to equal for the result

--- unittests_in_js/2-calcul_chai.test.js
+++ unittests_in_js/2-calcul_chai.test.js
@@ -5,18 +5,18 @@ const calculateNumber = require('./1-calcul.js');
     describe('Test the calculateNumber sum, subtract and divide', function() {
 
         it('should returnthe SUM OF ROUNDED NUMBER', function() {
-            expect(calculateNumber('sum', 1.4, 4.5), 6);
+            expect(calculateNumber('sum', 1.4, 4.5)).to.equal(6);
         });
     
         it('should return the Difference between a & b', function() {
-            expect(calculateNumber('sub', 1.4, 4.5), -4);
+            expect(calculateNumber('sub', 1.4, 4.5)).to.equal(-4);
         });
     
         it('should return the Division of a & b', function() {
-            expect(calculateNumber('division', 1.4, 4.5), 0.2);
+            expect(calculateNumber('division', 1.4, 4.5)).to.equal(0.2);
         });
     
         it('should return the Division by 0 shld throw error', function() {
-            expect(calculateNumber('division', 1.4, 0), 'Error');
+            expect(calculateNumber('division', 1.4, 0)).to.equal('Error');
         });
 });
