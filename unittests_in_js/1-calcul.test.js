b6991a2c29a7c2c0826fb08a8c54eff922bd44e2
seeyou7 <5764@holbertonstudents.com>
2024-11-13 11:37:49 +0100

    remove extra describe and

--- unittests_in_js/1-calcul.test.js
+++ unittests_in_js/1-calcul.test.js
@@ -1,24 +1,22 @@
 const assert = require('assert');
 const calculateNumber = require('./1-calcul.js');
 
-describe('calculateNumber', function() {
-
     describe('Test the calculateNumber sum, subtract and divide', function() {
 
         it('should returnthe SUM OF ROUNDED NUMBER', function() {
-            assert.equal(calculateNumber('sum', 1.4, 4.5), 6);
+            assert.strictEqual(calculateNumber('sum', 1.4, 4.5), 6);
         });
     
         it('should return the Difference between a & b', function() {
-            assert.equal(calculateNumber('sub', 1.4, 4.5), -4);
+            assert.strictEqual(calculateNumber('sub', 1.4, 4.5), -4);
         });
     
         it('should return the Division of a & b', function() {
-            assert.equal(calculateNumber('division', 1.4, 4.5), 0.2);
+            assert.strictEqual(calculateNumber('division', 1.4, 4.5), 0.2);
         });
     
         it('should return the Division by 0 shld throw error', function() {
-            assert.equal(calculateNumber('division', 1.4, 0), 'Error');
+            assert.strictEqual(calculateNumber('division', 1.4, 0), 'Error');
         });
 });
-});
+
