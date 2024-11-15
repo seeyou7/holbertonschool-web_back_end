bb85533cf951b91fa025d9affc29da4a5d7aa486
seeyou7 <5764@holbertonstudents.com>
2024-11-15 15:11:53 +0100

    add spy track

---
+++ unittests_in_js/utils.js
@@ -0,0 +1,24 @@
+const Utils = {
+    calculateNumber(type, a, b) {
+
+        // Round both numbers
+        const roundedA = Math.round(a);
+        const roundedB = Math.round(b);
+    
+        switch(type){
+            case 'sum' :
+                return roundedA + roundedB;
+            case 'sub' :
+                return roundedA - roundedB;
+            case 'division' :
+                if(roundedB === 0) {
+                    return "Error";
+                }
+                return roundedA / roundedB;
+            default:
+                throw new Error('Invalid operation type');
+        }
+    }
+};
+
+module.exports = Utils;
